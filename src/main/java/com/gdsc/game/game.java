182d38f3ce82e9627fd096fb1d6690031daac8ad
ohseongbin <dannybin814@naver.com>
2024-11-20 03:08:12 +0900

    inTurn

--- src/main/java/com/gdsc/game/game.java
+++ src/main/java/com/gdsc/game/game.java
@@ -1,6 +1,12 @@
 package com.gdsc.game;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Supplier;
+
+import static java.lang.Math.max;
+
 
 interface Turn{
     void inTurn(int choiceA, int choiceB); //턴 내부 상황
@@ -14,6 +20,12 @@ public class game implements Turn{
     private Character A;
     private Character B;
 
+    Action action = new Action();
+
+    private int[] actionList = {action.attack(),action.defense(),action.attack2(),action.attack3(),action.attackHard()};
+
+
+
     public game(Character A, Character B, int turn){ //매개변수 turn은 총 횟수
         this.A = A;
         this.B = B;
@@ -22,7 +34,27 @@ public class game implements Turn{
 
     @Override
     public void inTurn(int choiceA, int choiceB){
+        Action a = new Action();
+        Action b = new Action();
 
+        if(choiceA==2||choiceB==2){
+            if(choiceA==2&&choiceB==2) {return;} //둘다 방어일 경우 turn 넘김
+            else if (choiceA==2){ //A가 방어인 경우
+                int damage = max(0,actionList[choiceB]-actionList[1]); //damage는 음수일 수 없음
+                A.losehp(damage);
+            }
+            else{ //B가 방어인 경우
+                int damage = max(0,actionList[choiceA]-actionList[1]);
+                B.losehp(damage);
+            }
+        }
+        else{//둘다 공격인 경우
+            int damageA = actionList[choiceA]; //A가 먼저 공격
+            B.losehp(damageA);
+            if(B.getHp()<=0) return; // B죽음
+            int damageB = actionList[choiceB];
+            A.losehp(damageB);
+        }
     }
 
 
