898bade2fb64c05cff906e0544b21f74103cb927
ohseongbin <dannybin814@naver.com>
2024-11-21 01:31:37 +0900

    스킬 데미지 수정

--- src/main/java/com/gdsc/game/Action.java
+++ src/main/java/com/gdsc/game/Action.java
@@ -13,33 +13,38 @@ public class Action implements interfaceAction {
     @Override
     public int attack(){
         int damage = (int)(Math.random() * 10) + 1;
+        System.out.println("#:"+ damage); //test용
         return damage;
     }
 
     @Override
     public int defense(){
         int shield = (int)(Math.random() * 10) + 1;
+        System.out.println("#:"+ shield); //test용
         return shield;
 
     }
 
     @Override
     public int attack2(){
-        int damage = (int)(Math.random() * 19) + 2;
+        int damage = 2*((int)(Math.random() * 10) + 1);
+        System.out.println("#:"+ damage); //test용
         return damage;
 
     }
 
     @Override
     public int attack3(){
-        int damage = (int)(Math.random() * 28) + 3;
+        int damage = 3*((int)(Math.random() * 10) + 1);
+        System.out.println("#:"+ damage); //test용
         return damage;
 
     }
 
     @Override
     public int attackHard(){
-        int damage = (int)(Math.random() * 46) + 5;
+        int damage = 5*((int)(Math.random() * 10) + 1);
+        System.out.println("#:"+ damage); //test용
         return damage;
 
     }
