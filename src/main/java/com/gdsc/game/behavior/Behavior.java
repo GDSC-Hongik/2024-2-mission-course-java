d6e52afb8ebc14d0cee8a04ee1cdc99a84cbda2e
서가영 <alstpgml0812@gmail.com>
2024-11-23 23:32:38 +0900

    Feat: 행동 설명 추상 메서드 추가

--- src/main/java/com/gdsc/game/behavior/Behavior.java
+++ src/main/java/com/gdsc/game/behavior/Behavior.java
@@ -5,4 +5,5 @@ import com.gdsc.game.character.Character;
 // interface : 모든 행동(공격, 방어, 스킬)에 대한 공통 인터페이스
 public interface Behavior {
     void execute(Character myChar, Character opponentChar);
+    String getDescription();
 }
