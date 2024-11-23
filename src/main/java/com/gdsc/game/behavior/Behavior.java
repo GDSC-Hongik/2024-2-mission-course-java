5a323be86e896fffc279041ff0ca53dd4baad108
서가영 <alstpgml0812@gmail.com>
2024-11-23 23:01:05 +0900

    Feat: Add import

--- src/main/java/com/gdsc/game/behavior/Behavior.java
+++ src/main/java/com/gdsc/game/behavior/Behavior.java
@@ -1,6 +1,8 @@
 package com.gdsc.game.behavior;
 
+import com.gdsc.game.character.Character;
+
 // interface : 모든 행동(공격, 방어, 스킬)에 대한 공통 인터페이스
 public interface Behavior {
-    void execute(Character MyChar, Character OpponentChar);
+    void execute(Character myChar, Character opponentChar);
 }
