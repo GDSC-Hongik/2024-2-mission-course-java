26615a998f307c1666abe9a4474d36e93810577c
서가영 <alstpgml0812@gmail.com>
2024-11-23 23:31:43 +0900

    Feat: Add behavior code

--- src/main/java/com/gdsc/game/game/Game.java
+++ src/main/java/com/gdsc/game/game/Game.java
@@ -1,5 +1,7 @@
 package com.gdsc.game.game;
 
+import com.gdsc.game.behavior.Behavior;
+import com.gdsc.game.behavior.Skill;
 import com.gdsc.game.character.Character;
 
 import java.util.Scanner;
@@ -18,8 +20,10 @@ public class Game {
         Character character1 = new Character(names[0].trim(), 50, 30);
         Character character2 = new Character(names[1].trim(), 10, 5);
 
-        // 행동
-        // ......
+        // skill 생성하기
+        Skill slashTwo = new Skill("두번베기", 2, 20, 2, 0);
+        Skill slashThree = new Skill("3번베기", 3, 30, 3, 0);
+        Skill punch = new Skill("쎼게 때리기", 0, 5, 5, 2);
 
         System.out.println("몇 턴 동안 진행하시겠습니까?");
         int inputTurns = sc.nextInt();
@@ -33,7 +37,31 @@ public class Game {
                     character1.getName(), character1.getHp(), character1.getMp(),
                     character2.getName(), character2.getHp(), character2.getMp());
 
-            // 추가............
+            // 추가 ??? ............
+            System.out.println("행동을 선택하세요:");
+            for (int i = 0; i < character1.getBehaviorList().size(); i++) {
+                System.out.printf("%d. %s%n", i + 1, character1.getBehaviorList().get(i).getDescription());
+            }
+
+            int choice = sc.nextInt() - 1;
+            if (choice >= 0 && choice < character1.getBehaviorList().size()) {
+                character1.getBehaviorList().get(choice).execute(character1, character2);
+            }
+
+            // 기다리는 턴 수 감소
+            for (Behavior behavior : character1.getBehaviorList()) {
+                if (behavior instanceof Skill) {
+                    ((Skill) behavior).reduceCooldown();
+                }
+            }
+
+            // 방어값 초기화
+            character1.setDefense(0);
+
+            // 턴 교대
+            Character temp = character1;
+            character1 = character2;
+            character2 = temp;
         }
 
         // 결과 출력
