a3e8fda3cd484b341091ed2fea244d67386a5cf6
서가영 <alstpgml0812@gmail.com>
2024-11-23 23:00:47 +0900

    Feat: Add getter/setter

--- src/main/java/com/gdsc/game/character/Character.java
+++ src/main/java/com/gdsc/game/character/Character.java
@@ -18,7 +18,47 @@ public class Character {
         this.hp = hp;
         this.mp = mp;
         this.defense = 0;
-        // this.behaviorList = new ArrayList<>();
+        this.behaviorList = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public List<Behavior> getBehaviorList() {
+        return behaviorList;
+    }
+
+    public void setBehaviorList(List<Behavior> behaviorList) {
+        this.behaviorList = behaviorList;
     }
 
     // method: hp <= 0 이면 false 반환 (게임 종료)
