500f38f285a3cb6d28df9cf106331d0ade3b6021
서가영 <alstpgml0812@gmail.com>
2024-11-23 22:36:38 +0900

    Fix: import error

--- src/main/java/com/gdsc/game/behavior/Behavior.java
+++ src/main/java/com/gdsc/game/behavior/Behavior.java
@@ -1,5 +1,6 @@
+package com.gdsc.game.behavior;
+
 // interface : 모든 행동(공격, 방어, 스킬)에 대한 공통 인터페이스
 public interface Behavior {
     void execute(Character MyChar, Character OpponentChar);
-
 }
--- src/main/java/com/gdsc/game/character/Character.java
+++ src/main/java/com/gdsc/game/character/Character.java
@@ -1,3 +1,10 @@
+package com.gdsc.game.character;
+
+import com.gdsc.game.behavior.Behavior;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Character {
     String name;
     int hp;
@@ -10,8 +17,8 @@ public class Character {
         this.name = name;
         this.hp = hp;
         this.mp = mp;
-        this.defense = 0
-        this.behaviorList = new ArrayList<>();
+        this.defense = 0;
+        // this.behaviorList = new ArrayList<>();
     }
 
     // method: hp <= 0 이면 false 반환 (게임 종료)
