f698065cbd499cdb446ee61dd8674b72b4af8581
서가영 <alstpgml0812@gmail.com>
2024-11-23 22:15:27 +0900

    Feat: Generate Character class

---
+++ src/main/java/com/gdsc/game/character/Character.java
@@ -0,0 +1,22 @@
+public class Character {
+    String name;
+    int hp;
+    int mp;
+    int defense;
+    List<Behavior> behaviorList;
+
+    // constructor
+    public Character(String name, int hp, int mp) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+        this.defense = 0
+        this.behaviorList = new ArrayList<>();
+    }
+
+    // method: hp <= 0 이면 false 반환 (게임 종료)
+    boolean isAlive() {
+        return this.hp > 0;
+    }
+
+}
