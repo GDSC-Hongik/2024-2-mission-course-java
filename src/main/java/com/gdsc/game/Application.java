0f69d339c8e0d8a91d49daf094a0296fb0995a3e
mirupio <162397816+mirupio@users.noreply.github.com>
2024-11-30 23:25:36 +0900

    Application

--- src/main/java/com/gdsc/game/Application.java
+++ src/main/java/com/gdsc/game/Application.java
@@ -1,11 +1,17 @@
 package com.gdsc.game;
 
 import com.gdsc.game.model.Character;
+import com.gdsc.game.model.Game;
+import com.gdsc.game.model.Skill;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
+@SpringBootApplication
 public class Application {
+    /*
     public static void main(String[] args) throws UnsupportedEncodingException {
         System.setOut(new PrintStream(System.out, true, "UTF-8"));
 
@@ -17,8 +23,8 @@ public class Application {
         Skill ice_arrow = new Skill("얼음 화살", 3, 1);
 
         // 캐릭터 생성
-        com.gdsc.game.model.Character knight = new com.gdsc.game.model.Character("knight", 50, 30, new Skill[]{double_strike, triple_strike, power_strike});
-        com.gdsc.game.model.Character slime = new Character("slime", 30, 20, new Skill[]{fire_arrow, ice_arrow});
+        Character knight = new Character("knight", 50, 30, new Skill[]{double_strike, triple_strike, power_strike});
+        Character slime = new Character("slime", 30, 20, new Skill[]{fire_arrow, ice_arrow});
 
         // 게임 생성
         Game game = new Game(knight, slime, 5);
@@ -27,5 +33,9 @@ public class Application {
         while (!game.isGameOver()) {
             game.nextTurn();
         }
+    }
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
